7d247abda2f53b62fd32a9bba737e0ddeb2db3a7
Ivan Monzon <i.monzon@LIXISNET.COM>
2022-04-15 18:23:43 -0300

    beginning

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,17 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const  authRouter  = require('./auth');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/auth', authRouter);
+
+router.get('/', (req, res) => {
+    res.send('Root Route');
+});
 
 
 module.exports = router;
